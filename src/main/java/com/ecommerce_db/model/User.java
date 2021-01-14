1bd7165349d3ec0a845c15797fa309cab5b84311
CundullahT <cundullahterzioglu@gmail.com>
2021-01-14 12:35:53 +0100

    Initial Commit

--- src/main/java/com/ecommerce_db/model/User.java
+++ src/main/java/com/ecommerce_db/model/User.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -14,8 +15,13 @@ import javax.persistence.Table;
 @Table(name = "users")
 public class User extends BaseEntity {
 
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false, unique = true)
     private String email;
 
 }
